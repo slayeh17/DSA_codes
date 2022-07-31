6c449bba105d5191fbae084014281954939255d3
Debak Roy <104914725+slayeh17@users.noreply.github.com>
2022-07-31 21:45:10 +0530

    Update Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -35,7 +35,7 @@ void recursive_sorted_insertion(node **head,int n)//2
         *head=temp;
     }
     else
-    recursive_sorted_insertion(&((*head)->next),n);  
+    recursive_sorted_insertion(&((*head)->next),n);
 }
 void sequential_insertion(node **head,int n)//3
 {
@@ -59,6 +59,7 @@ void recursive_sequential_insertion(node **head,int n)//4
     *head=temp;
     else
     recursive_sequential_insertion(&((*head)->next),n);
+
 }
 void display(node *head)//5
 {
@@ -66,7 +67,7 @@ void display(node *head)//5
     {
         printf("%d ",head->data);
         head=head->next;
-    }   
+    }
 }
 void recursive_display(node *head)//6
 {
@@ -141,7 +142,7 @@ void split_even_odd(node **head,node *heade,node *heado)//12
 }
 void merge_sorted(node *head1,node *head2,node *headnew)//13
 {
-    node *cur1,*cur2,*curnew;
+    node *cur1,*cur2,*curnew,*cur;
     for(cur1=head1,cur2=head2;cur1!=NULL&&cur2!=NULL;)
     {
         if(cur1->data<cur2->data)
@@ -155,10 +156,9 @@ void merge_sorted(node *head1,node *head2,node *headnew)//13
             cur2=cur2->next;
         }
     }
-    for(;cur1!=NULL;cur1=cur1->next)
-    sequential_insertion(&headnew,cur1->data);
-    for(;cur2!=NULL;cur2=cur2->next)
-    sequential_insertion(&headnew,cur2->data);
+    cur=cur1!=NULL?cur1:cur2;
+    for(;cur!=NULL;cur=cur->next)
+    sequential_insertion(&headnew,cur->data);
     display(headnew);
 }
 void search(node *head,int n)//14
@@ -182,28 +182,28 @@ int main()
     ini(&head2);
     int n,d;
     scanf("%d",&n);
+    // while(n--)
+    // {
+    //     scanf("%d",&d);
+    //     recursive_sequential_insertion(&head,d);
+    // }
     while(n--)
     {
         scanf("%d",&d);
-        recursive_sequential_insertion(&head,d);
+        recursive_sorted_insertion(&head1,d);
     }
-    // while(n--)
-    // {
-    //     scanf("%d",&d);
-    //     recursive_sorted_insertion(&head1,d);
-    // }
-    // scanf("%d",&n);
-    // while(n--)
-    // {
-    //     scanf("%d",&d);
-    //     recursive_sorted_insertion(&head2,d);
-    // }
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d",&d);
+        recursive_sorted_insertion(&head2,d);
+    }
     // int s;
     // scanf("%d",&s);
     // search(head,s);
     //reverse(&head);
     //recursive_reverse_display(head);
-    //merge_sorted(head1,head2,NULL);
+    merge_sorted(head1,head2,NULL);
     //int c= recursive_count(head);
     //printf("\nNumber of elements in the Linked List: %d",c);
     // delete_node(&head,69);
